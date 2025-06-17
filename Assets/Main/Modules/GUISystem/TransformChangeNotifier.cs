8c3cdbfcc4a683f6bc9d5ab4831d057853febc9e
Sou Tanaka <110368284+neko-kurage@users.noreply.github.com>
2025-06-18 03:13:29 +0900

    Refactor transform change system

---
+++ Assets/Main/Modules/GUISystem/TransformChangeNotifier.cs
@@ -0,0 +1,109 @@
+using System;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace GUISystem
+{
+    /// <summary>
+    /// Transformの変化を検知しイベントを発行するコンポーネントです。
+    /// </summary>
+    [ExecuteAlways]
+    public class TransformChangeNotifier : MonoBehaviour
+    {
+        /// <summary>
+        /// 変化時に通知されるイベントです。
+        /// </summary>
+        public TransformUnityEvent OnTransformChanged => _onTransformChanged;
+
+        [SerializeField]
+        private TransformUnityEvent _onTransformChanged = new();
+
+        private TransformState _state;
+        private bool _hierarchyDirty;
+
+        void Awake()
+        {
+            _state = new TransformState(transform);
+        }
+
+        void OnTransformParentChanged()
+        {
+            MarkDirty();
+        }
+
+        void OnTransformChildrenChanged()
+        {
+            MarkDirty();
+        }
+
+#if UNITY_EDITOR
+        void OnEnable()
+        {
+            EditorApplication.hierarchyChanged += MarkDirty;
+        }
+
+        void OnDisable()
+        {
+            EditorApplication.hierarchyChanged -= MarkDirty;
+        }
+#endif
+
+        void LateUpdate()
+        {
+            if (_state.HasChanged(transform))
+            {
+                _state.Capture(transform);
+                _onTransformChanged.Invoke(transform);
+            }
+
+            if (_hierarchyDirty)
+            {
+                _hierarchyDirty = false;
+                _state.Capture(transform);
+                _onTransformChanged.Invoke(transform);
+            }
+        }
+
+        /// <summary>
+        /// 変化通知を強制的に送信します。
+        /// </summary>
+        public void ForceNotify()
+        {
+            _state.Capture(transform);
+            _onTransformChanged.Invoke(transform);
+        }
+
+        /// <summary>
+        /// リスナーを登録します。
+        /// </summary>
+        /// <param name="handler">ハンドラー</param>
+        public void RegisterHandler(ITransformChangeHandler handler)
+        {
+            _onTransformChanged.AddListener(handler.OnTransformChanged);
+        }
+
+        /// <summary>
+        /// リスナーを解除します。
+        /// </summary>
+        /// <param name="handler">ハンドラー</param>
+        public void UnregisterHandler(ITransformChangeHandler handler)
+        {
+            _onTransformChanged.RemoveListener(handler.OnTransformChanged);
+        }
+
+        void MarkDirty()
+        {
+            _hierarchyDirty = true;
+        }
+    }
+
+    /// <summary>
+    /// Transformを引数とするUnityEventです。
+    /// </summary>
+    [Serializable]
+    public class TransformUnityEvent : UnityEvent<Transform>
+    {
+    }
+}
